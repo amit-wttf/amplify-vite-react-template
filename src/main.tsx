eed1f19aad66d7048edca206c4af1ed78c1bc5e3
intothejg <86368599+intothejg@users.noreply.github.com>
2025-03-10 21:13:43 -0400

    added per-user data isolation

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,8 @@ import { Authenticator } from "@aws-amplify/ui-react";
 import { Amplify } from "aws-amplify";
 import outputs from "../amplify_outputs.json";
 
+import "@aws-amplify/ui-react/styles.css";
+
 Amplify.configure(outputs);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
